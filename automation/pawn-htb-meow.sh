7e5560beab871f01e07b18ca90410d7f2596950f
ranjananjadhav12@gmail.com <ranjananjadhav12@gmail.com>
2026-02-21 03:05:17 -0500

    pawn-htb-meow

--- automation/pawn-htb-meow.sh
+++ automation/pawn-htb-meow.sh
@@ -9,9 +9,9 @@ if [ -z "$TARGET" ] ; then
     echo "usage : $0 <target_ip>"
     exit 1
 fi
-#echo "[*] HTB meow Auto-exploit-target: $TARGET "
-#echo "[+] Checking connectivity to target. . ."
-#ping -c 2 -w 2 $TARGET &>/dev/null && echo "[*] Alive " || echo "[x] Dead"; exit 1 
+echo "[*] HTB meow Auto-exploit-target: $TARGET "
+echo "[+] Checking connectivity to target. . ."
+ping -c 2 -w 2 $TARGET &>/dev/null && echo "[*] Alive " || echo "[x] Dead"; exit 1 
 echo "[+] Scanning Port 23 . . ."
 if nmap -p 23 --open $TARGET 2>/dev/null | grep -q "23/tcp"; then 
     echo "[*] Open " 
